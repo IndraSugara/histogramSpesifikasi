8a3adcc07a67ea10282c73821b06bc110d7e56dc
Maqlogemink <sughara78@gmail.com>
2026-04-22 14:22:15 +0700

    mapping hasil

--- histogram_spec.py
+++ histogram_spec.py
@@ -43,3 +43,65 @@ def calculate_target_table(pz, L):
         'cdf': cdf.tolist(),
         'vk': vk.tolist(),
     }
+    
+
+def compute_mapping(source_table, target_table, L):
+    sk_values = source_table['sk']
+    vk_values = target_table['vk']
+    
+    mapping = {}
+    
+    for rk in range(L):
+        sk = sk_values[rk]
+        
+        min_diff = float('inf')
+        best_zk = 0
+        
+        for zk in range(L):
+            vk = vk_values[zk]
+            diff = abs(sk - vk)
+            
+            if diff < min_diff:
+                min_diff = diff
+                best_zk = zk
+        mapping[rk] = best_zk
+    
+    return mapping
+
+
+def apply_mapping(image_array, mapping):
+    image = np.array(image_array, dtype=int)
+    result = np.zeros_like(image)
+    
+    for rk, zk in mapping.items():
+        result[image == rk] = zk
+    
+    return result
+
+
+def calculate_result_histogram(mapping, source_table, L):
+    result_hist = [0] * L
+    
+    for rk, zk in mapping.items():
+        result_hist[zk] += int(source_table['frekuensi'][rk])
+    
+    return result_hist
+
+
+def run_histogram_specification(nk, pz, L, n):
+    source_table = calculate_source_table(nk, L, n)
+    
+    target_table = calculate_target_table(pz, L)
+    
+    mapping = compute_mapping(source_table, target_table, L)
+
+    result_histogram = calculate_result_histogram(mapping, source_table, L)
+    
+    return {
+        'source_table': source_table,
+        'target_table': target_table,
+        'mapping': mapping,
+        'result_histogram': result_histogram,
+        'L': L,
+        'n': n,
+    }
